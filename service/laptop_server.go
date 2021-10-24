87042c3ddda8c1606b1ea98eb55081b2b34f9fdb
maniizu3110 <maniizu3110@gmail.com>
2021-10-24 15:54:19 +0900

    interfaceが作られない

---
+++ service/laptop_server.go
@@ -0,0 +1,11 @@
+package service
+
+//LaptopServer is the server that provides laptop services
+type LaptopServer struct {
+}
+
+func NewLaptopServer() *LaptopServer {
+	return &LaptopServer{}
+}
+
+//func ()
